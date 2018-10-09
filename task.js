e543cd63d6b928e85f1fa9defce92f3aa88c72e3
Jose Hernandez-Inunza <>
2018-10-09 11:36:59 -0600

    starting task object skeleton

---
+++ task.js
@@ -0,0 +1,13 @@
+function task (user, complete, name, due, added, id, desc) {
+  userID = 1;
+  completed = false;
+  taskName = "";
+  dueDate = "";
+  addedDate = "";
+  taskID = 0;
+  description = '';
+};
+
+
+// TESTING GROUND //
+var taskNumber1 = new task(1, false, "", "", "", 0, "") ;
