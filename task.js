030dd8743cdf1724f00309fa66aa25310071f9be
Jose Hernandez-Inunza <>
2018-10-09 16:10:49 -0600

    merged in master updates

---
+++ task.js
@@ -0,0 +1,21 @@
+function task (name, due, desc) {
+  userID = null;
+  completed = false;
+  taskID = null;
+
+  var dateAdded = new Date();
+  taskName = name;
+  dueDate = due;
+  description = desc;
+  addedDate = dateAdded.toDateString();
+};
+
+
+// TESTING GROUND //
+var taskNumber1 = new task("Task 1", "Tomorrow", "This is task number 1") ;
+
+var customName = $("#tempTaskName");
+customName.text("My Custom Task Title");
+
+var customDueDate = $("#tempDueDate");
+customDueDate.text("Due Tomorrow");
