202936aba72de033d225844ecda7302ee1efc192
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2018-10-10 11:22:04 -0600

    local storage working including null cases for due date values

--- task.js
+++ task.js
@@ -5,7 +5,7 @@ function Task(taskName, dueDate, description, taskID) {
   //be sure to switch the taskID designations when we hook up a real database
 
   this.taskName = taskName;
-  this.dueDate = new Date(dueDate.toString());
+  this.dueDate = new Date(dueDate.toString()) || null;
   this.description = description;
   this.taskID = taskID;
   this.addedDate = new Date();
@@ -16,6 +16,7 @@ function Task(taskName, dueDate, description, taskID) {
 var taskNumber1 = new Task("Task 1", "october 10 2017", "This is task number 1" , 1) ;
 var taskNumber2 = new Task("Name", "feb 17 2018", "This is task number 2" , 2) ;
 var taskNumber3 = new Task("Three", "nov 17 2018", "This is task number 3" , 3) ;
+var taskNumber4 = new Task("Four", "" , "This is task number 4" , 5) ;
 //
 // var customName = $("#tempTaskName");
 // customName.text("My Custom Task Title");
