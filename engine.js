cb2d22e64ddf96b2331147ec3ae94d87279c7595
Romero Gossom <>
2018-10-10 11:50:43 -0600

    Still working on task

--- engine.js
+++ engine.js
@@ -5,6 +5,18 @@ function Tasklist(){
 //add tasks
 
 //delete tasks
+Tasklist.prototype.deleteTask = function(Task){
+  var gone = [];
+  for(var index=0; index<this.list.length; index++) {
+    console.log("task have been deleted");
+    if(this.list[index].Task === Task){
+      this.list.splice(index);
+     console.log(gone);
+    }
+  }
+
+};
+
 
 //edit tasks
 
